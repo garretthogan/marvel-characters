06ec247e182c62007bde4daedf1af1feebc4a8f3
Garrett Hogan <garrett.hogan.dev@gmail.com>
2019-05-11 13:07:25 -0700

    Updates

---
+++ precache-manifest.6c6a3be4f50a4204d4e56fbc1727b9d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60ce7bb12d07d6afb94f22c43326ccd",
+    "url": "/marvel-characters/index.html"
+  },
+  {
+    "revision": "5ee93191abaf9dc3c353",
+    "url": "/marvel-characters/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "712906ee2750975c0c40",
+    "url": "/marvel-characters/static/js/2.3eb0df5e.chunk.js"
+  },
+  {
+    "revision": "5ee93191abaf9dc3c353",
+    "url": "/marvel-characters/static/js/main.3b4f1123.chunk.js"
+  },
+  {
+    "revision": "e46c73d2a3e37d4a2e0e",
+    "url": "/marvel-characters/static/js/runtime~main.5c5e3208.js"
+  }
+]);
